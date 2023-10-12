dc0586c619772d0b6a8246a80e5cee1e84da421f
Neeli Akash <neeli.akash@indiamart.com>
2023-10-12 12:11:51 +0530

    Few Changes

--- lib/LoginFlow/DetailsRequest.dart
+++ lib/LoginFlow/DetailsRequest.dart
@@ -1,7 +1,10 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_tests/view_categories.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:keyboard_dismisser/keyboard_dismisser.dart';
@@ -32,6 +35,23 @@ class DetailsRequestState extends State<DetailsRequest> {
     emailTextField.dispose();
     pincodeTextField.dispose();
     super.dispose();
+  }
+
+  updateDetails() {
+    var updatedUsing = "";
+
+    if (Platform.isIOS) {
+      updatedUsing = "Edit Profile (IOS)";
+    } else {
+      updatedUsing = "Edit Profile (Android)";
+    }
+    String pathUrl =
+        "https://mapi.indiamart.com/wservce/users/edit/?USR_ID=199718898&VALIDATION_GLID=199718898&APP_SCREEN_NAME=OtpEnterMoreDetails&IP=49.36.221.59&FIRSTNAME=mnb&AK=eyJ0eXAiOiJKV1QiLCJhbGciOiJzaGEyNTYifQ.eyJpc3MiOiJVU0VSIiwiYXVkIjoiMSo1KjQqNyo4KiIsImV4cCI6MTY5NjYwMDc4OCwiaWF0IjoxNjk2NTE0Mzg4LCJzdWIiOiIxOTk3MTg4OTgiLCJjZHQiOiIwNS0xMC0yMDIzIn0.uJIAduzC6Q6e3ED63IQb9GMpRbs3A-lZB16ZYLs5BJU&EMAIL=ghahah@gmail.com&UPDATEDUSING=Edit Profile (Android)&APP_USER_ID=199718898&APP_MODID=ANDROID&VALIDATION_KEY=e27d039e38ae7b3d439e8d1fe870fc68&APP_ACCURACY=0.0&APP_LATITUDE=0.0&IP_COUNTRY=India&APP_LONGITUDE=0.0&VALIDATION_USER_IP=49.36.221.59&UPDATEDBY=User&app_version_no=13.2.2_T1&VALIDATION_USERCONTACT=1455487885&MODID=ANDROID";
+  }
+
+  proceedToHome() {
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => ViewCategories()));
   }
 
   Future<bool> handleLocationPermission() async {
@@ -278,11 +298,13 @@ class DetailsRequestState extends State<DetailsRequest> {
                             padding: const EdgeInsets.fromLTRB(20, 10, 100, 20),
                             child: TextField(
                               focusNode: pinCodeTextFieldFocus,
-                              textInputAction: TextInputAction.done,
+                              textInputAction: TextInputAction.next,
                               keyboardType: TextInputType.number,
                               autocorrect: false,
                               onSubmitted: (value) {
                                 // keyboard done action
+                                FocusScope.of(context).unfocus();
+                                proceedToHome();
                               },
                               onChanged: (searchingText) {},
                               onEditingComplete: () {},
@@ -306,6 +328,7 @@ class DetailsRequestState extends State<DetailsRequest> {
                   GestureDetector(
                     onTap: () {
                       print("Next tapped");
+                      proceedToHome();
                     },
                     child: Container(
                       height: 50,
